c8ba7189f9dbd92d45a3b485b30684c0570d2653
Erdi Muliqi <erdieroni@gmail.com>
2022-05-02 23:02:35 +0200

    No length check (not necessary) and no console.log

--- src/showlist.js
+++ src/showlist.js
@@ -1,9 +1,6 @@
 setInterval(() => {
     const cards = document.getElementsByClassName("js-num-cards");
 
-    if(cards.length > 0)
-        for(const card of cards)
-            card.classList.remove("hide");
-
-    console.log(cards);
+    for(const card of cards)
+        card.classList.remove("hide");
 }, 1000);
